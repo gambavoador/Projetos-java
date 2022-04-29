a8af68e099882cb6180ae4007577c834527531fc
gambavoador <ric.rov9@gmail.com>
2022-04-28 21:41:54 -0300

    jogo da velha

---
+++ JogoDaVelhaJava/src/main/java/com/mycompany/jogodavelhajava/JogoDaVelhaJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.jogodavelhajava;
+
+/**
+ *
+ * @author ricardo.vieira
+ */
+public class JogoDaVelhaJava {
+
+    public static void main(String[] args) {
+        Jogo FrameJogo=new Jogo();
+        FrameJogo.setVisible(true);
+    }
+}
